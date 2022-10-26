1ae079369a41cd4737e8bfae1133661ffc95e42f
CrLn-66 <78108570+CrLn-66@users.noreply.github.com>
2022-10-27 00:12:53 +0800

    Add files via upload

--- CredentialGUI/src/gui/GUI.java
+++ CredentialGUI/src/gui/GUI.java
@@ -1,5 +1,10 @@
 package gui;
 
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -8,7 +13,16 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
-public class GUI implements ActionListener{
+public class GUI extends Canvas implements ActionListener{
+	
+	@Override
+	public void paint(Graphics g) {
+		// TODO Auto-generated method stub
+		Toolkit t = Toolkit.getDefaultToolkit();
+		Image im = t.getImage("C:\\\\Users\\\\Abrasaldo\\\\eclipse-workspace\\\\CredentialGUI\\\\src\\\\gui\\\\ca.jpg");
+		g.drawImage(im, 100,30,this);  
+	}
+	
 	static JPasswordField pass;
 	static JButton button;
 	static JLabel res;
@@ -19,25 +33,35 @@ public class GUI implements ActionListener{
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
 		JLabel label = new JLabel("Password : ");
+		Image icon = Toolkit.getDefaultToolkit().getImage("C:\\Users\\Abrasaldo\\eclipse-workspace\\CredentialGUI\\src\\gui\\as.jpg");
+		frame.setIconImage(icon);
 		name = new JLabel("");
 		section = new JLabel("");
 		age = new JLabel("");
 		name.setBounds(20, 320, 200, 10);
 		section.setBounds(20, 350, 100, 10);
 		age.setBounds(20, 380, 100, 15);
+		GUI gui = new GUI();
+		gui.setSize(200, 200);
+		panel.add(gui);
 		panel.add(name);
 		panel.add(section);
 		panel.add(age);
 		res = new JLabel();
 		res.setBounds(200, 100, 150,80);
-		button = new JButton("Login");
-		button.setBounds(75, 250, 100, 25);
+		button = new JButton("<html><font color='black'>Log In</font></html>");
+		button.setBounds(200, 230, 100, 25);
+		button.setBackground(Color.WHITE);
+		panel.setBackground(Color.GREEN);
 		button.addActionListener(new GUI());
 		pass = new JPasswordField();
 		pass.setBounds(130, 200, 250, 25);
 		panel.add(button);
 		panel.add(res);
 		panel.add(pass);
+		JLabel labels = new JLabel("Created By : BNDCT");
+		labels.setBounds(350, 440, 200, 20);
+		panel.add(labels);
 		label.setBounds(60, 200, 80, 25);
 		panel.setLayout(null);
 		panel.add(label);
@@ -46,6 +70,7 @@ public class GUI implements ActionListener{
 		frame.setSize(500,500);
 		frame.setVisible(true);
 		frame.setResizable(false);
+		frame.setIconImage(icon);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 	}
@@ -60,10 +85,10 @@ public class GUI implements ActionListener{
 		// TODO Auto-generated method stub
 		try {
 		if(User.login(new String(pass.getPassword()))) {
-			res.setText("<html><font color='green'>Successful Login!</font></html>");
-			name.setText("<html><font color='blue'>Name : Benedict V. Abrasaldo Jr.</font></html>");
-			section.setText("<html><font color='blue'>Section : Flexible</font></html>");
-			age.setText("<html><font color='blue'>Age : 16</font></html>");
+			res.setText("<html><font color='white' face='Times New Roman'>Successful Login!</font></html>");
+			name.setText("<html><font color='blue' face='WildWest'>Name : Benedict V. Abrasaldo Jr.</font></html>");
+			section.setText("<html><font color='blue' face='WildWest'>Section : Flexible</font></html>");
+			age.setText("<html><font color='blue' face='WildWest'>Age : 16</font></html>");
 			
 		}else {
 			res.setText("<html><font color='red'>Incorrect Password.</font></html>");
--- CredentialGUI/src/gui/Testt.java
+++ CredentialGUI/src/gui/Testt.java
@@ -5,11 +5,9 @@ import java.security.MessageDigest;
 public class Testt {
 
 	public static void main(String[] args) throws Exception{
-		//This is test class. wala kang mahahanap dito :)
-		/**
 		// TODO Auto-generated method stub
 		MessageDigest ms = MessageDigest.getInstance("SHA-256");
-		ms.update("Benedictc//ut//e".getBytes());
+		ms.update("Benedictcute".getBytes());
 		//System.out.println(new String(ms.digest()));
 		StringBuffer hexString = new StringBuffer();
 	    byte[] b = ms.digest();
@@ -17,7 +15,6 @@ public class Testt {
 	         hexString.append(Integer.toHexString(0xFF & b[i]));
 	      }
 	      System.out.println(hexString.toString());
-	      **/
 	}
 
 }
